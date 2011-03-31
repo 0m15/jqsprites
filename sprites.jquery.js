db2e19e45e6223c032b59ad381c10044bbf73f6f
Simone <simonecarella@gmail.com>
2011-03-31 19:06:47 +0200

    added animation support

--- sprites.jquery.js
+++ sprites.jquery.js
@@ -41,38 +41,53 @@
             className, 
             src, 
             width, 
-            height;
+            height,
+            animation,
+            current;
             
         var settings = {
-            'defaultWidth'  : 64,
-            'defaultHeight' : 64,
+            'defaultWidth'  : 40,
+            'defaultHeight' : 60,
         };
                 
-   	function parseQs(qs) {
-	    var ex,
-	         a = /\+/g,  // Regex for replacing addition symbol with a space
-		 r = /([^&=]+)=?([^&]*)/g,
-		 d = function (s) { return decodeURIComponent(s.replace(a, " ")); },
-		 q = qs,
-                 tmp = {};
+   	    function parseQs(qs) {
+            var ex,
+                a = /\+/g,  // Regex for replacing addition symbol with a space
+                r = /([^&=]+)=?([^&]*)/g,
+                d = function (s) { return decodeURIComponent(s.replace(a, " ")); },
+                q = qs,
+                tmp = {};
 
-            while (ex = r.exec(q)) {
-                tmp[d(ex[1])] = d(ex[2]);
-            }	
-            return tmp;
-	}
+                while (ex = r.exec(q)) {
+                    tmp[d(ex[1])] = d(ex[2]);
+                }	
+                console.log(tmp);
+                return tmp;
+    	}
+    	
+    	function stop() {
+    	    console.log('======== stop');
+    	    window.clearInterval(animation);
+    	    
+    	}
+    	
+    	function animate(e, offset, current, x, y) {
+             console.log('offset: ' + (current + offset));
+             console.log('current: ' + current);
+             e.style.background = 'url(' + src + ') -' + current + 'px -' + y + 'px ';
+    	}
 		
         if (options) {
             $.extend(settings, options);
         }
         
         return this.each(function() {
-            var self = $(this), x, y, hx, hy;
+            var self = $(this), x, y, hx, hy, end, fps, timeout, frame = 1, offset, loop;
             params = parseQs(self.attr('src').split('?')[1]);
             src = self.attr('src').split('?')[0];
             x = params['x'];
             y = params['y'];
-	    width = self.attr('width') || settings.defaultWidth;
+	        width = self.attr('width') || settings.defaultWidth;
             height = self.attr('height') || settings.defaultHeight;
 			className = self.attr('class');
             
@@ -86,15 +101,55 @@
 
             if (params['hx'] || params['hy']) {
                 hx = params['hx'];
-		hy = params['hy'];
+                hy = params['hy'];
 			
-		$(e).bind('mouseover', function() {
-		    e.style.background = 'url(' + src + ') -' + hx + 'px -' + hy + 'px ';
-		});
+                $(e).bind('mouseover', function() {
+                    e.style.background = 'url(' + src + ') -' + hx + 'px -' + hy + 'px ';
+                });
 				
-		$(e).bind('mouseout', function() {
-		    e.style.background = 'url(' + src + ') -' + x + 'px -' + y + 'px ';
-		});
+                $(e).bind('mouseout', function() {
+                    e.style.background = 'url(' + src + ') -' + x + 'px -' + y + 'px ';
+                });
+            }
+            
+            
+            
+            if(params['animate'] && params['offset'] && params['end'] && params['fps']) {
+                // fps to timeout = 1000 ms / number of frames
+                // 1000 / 25 = 40 ms
+                timeout = 1000 / parseInt(params['fps']);
+                console.log(timeout);
+                end = parseInt(params['end']);
+                current = parseInt(x);
+                offset = parseInt(params['offset']);
+                frame += 1;
+                loop = params['loop'];
+
+                animation = window.setInterval(function() {
+                     //console.log(parseInt(params['offset']));
+                     //offset = parseInt(x);
+                     current = current + offset;
+                     console.log('loop: ' + loop);
+                     
+                     if (current > end && loop) {
+                         console.log('========= end')
+                         //stop();
+                         //window.clearInterval(animation);
+                         //animate(e, offset, current, x, y);
+                         current = parseInt(x);
+                         
+                     } else if (current == end && !loop) {
+                         window.clearInterval(animation);
+                     }
+                     
+                     animate(e, offset, current, x, y);
+                     
+                }, timeout);
+                
+                                
+
+                
+                
             }
         }); 
     }
